2b946f4b3d168a8e9ecb716a08985b9b9316d8e4
Joosemi02 <josemiguel.dark@gmail.com>
2023-04-18 01:13:37 +0200

    Created moderation cog with kick command

---
+++ cogs/moderation.py
@@ -0,0 +1,28 @@
+import discord
+from discord import Interaction, app_commands
+from discord.errors import Forbidden
+from discord.ext import commands
+
+from utils import embed_fail, embed_success, _T
+
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.bot.user.name}: Moderation extension loaded successfully.")
+
+    @app_commands.command()
+    async def kick(self, i: Interaction, member: discord.Member, reason: str = None):
+        await i.response.defer()
+        try:
+            await member.kick(reason=reason)
+        except Forbidden:
+            await i.followup.send(embed_fail(_T(i, "command_fail.forbidden")))
+        await i.followup.send(embed_success(_T(i, "moderation.kick", member=member.display_name)))
+
+
+async def setup(bot):
+    await bot.add_cog(Moderation(bot))
