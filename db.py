b5824d27db87bd692ca155ccd0077b2c51ce7bfa
Joosemi02 <josemiguel.dark@gmail.com>
2023-04-18 01:00:47 +0200

    Add languages cache to db

--- db.py
+++ db.py
@@ -1,17 +1,34 @@
+import json
+import os
+
 from motor import motor_tornado
 
 from constants import DEFAULT_GUILD_SETTINGS, MONGODB_CONNECTION_STR
 
 
+def load_languages():
+    translations = {}
+    for filename in os.listdir("langs"):
+        if filename.endswith(".json"):
+            lang = filename[:-5]
+            with open(f"translations/{filename}", "r") as f:
+                translations[lang] = json.load(f)
+    return translations
+
+
 class Database:
     async def __init__(self, url):
+        # Collections
         db = motor_tornado.MotorClient(url)["security"]
         self.guilds = db.guilds
 
+        # Cache
         prefs = self.guilds.find({})
         self.guilds_cache = {
             d["_id"]: {k: v for k, v in d.items() if k != "_id"} async for d in prefs
         }
+
+        self.translations = load_languages()
 
     def get_guild_lang(self, guild_id: int):
         return self.guilds_cache[guild_id]["lang"]
