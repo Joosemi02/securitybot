ffade5ef53bc3528a9d946675fb3d271c4baac4b
Joosemi02 <josemiguel.dark@gmail.com>
2023-04-18 20:04:08 +0200

    - Created ban command - Log punishments

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -3,7 +3,7 @@ from discord import Interaction, app_commands
 from discord.errors import Forbidden
 from discord.ext import commands
 
-from utils import _T, embed_fail, embed_success, is_mod
+from utils import _T, embed_fail, embed_success, is_mod, log
 
 
 class Moderation(commands.Cog):
@@ -15,16 +15,42 @@ class Moderation(commands.Cog):
         print(f"{self.bot.user.name}: Moderation extension loaded successfully.")
 
     @app_commands.command()
-    @app_commands.check()
+    @app_commands.check(is_mod)
     async def kick(self, i: Interaction, member: discord.Member, reason: str = None):
         await i.response.defer()
         try:
             await member.kick(reason=reason)
         except Forbidden:
             await i.followup.send(embed_fail(_T(i, "command_fail.forbidden")))
-        await i.followup.send(
-            embed_success(_T(i, "moderation.kick", member=member.display_name))
+
+        punishment_msg = _T(
+            i,
+            "moderation.kick",
+            member=member.display_name,
+            reason=f"for {reason}" or "✅",
         )
+
+        await i.followup.send(embed_success(punishment_msg))
+        await log(i, punishment_msg)
+
+    @app_commands.command()
+    @app_commands.check(is_mod)
+    async def ban(self, i: Interaction, member: discord.Member, reason: str = None):
+        await i.response.defer()
+        try:
+            await member.kick(reason=reason)
+        except Forbidden:
+            await i.followup.send(embed_fail(_T(i, "command_fail.forbidden")))
+
+        punishment_msg = _T(
+            i,
+            "moderation.ban",
+            member=member.display_name,
+            reason=f"for {reason}" or "✅",
+        )
+
+        await i.followup.send(embed_success(punishment_msg))
+        await log(i, punishment_msg)
 
 
 async def setup(bot):
