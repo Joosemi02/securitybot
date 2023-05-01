090d9d1f74cb9529a47813a6249df40541637431
Joosemi02 <josemiguel.dark@gmail.com>
2023-05-01 06:40:47 +0200

    Temp changes to be able to run the bot locally

--- constants.py
+++ constants.py
@@ -1,8 +1,14 @@
-MONGODB_CONNECTION_STR = ""
-APPLICATION_ID = 000000000000
-TOKEN = ""
+import os
 
-ADMINS = []
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_CONNECTION_URI = os.getenv("MONGO")
+APPLICATION_ID = int(os.getenv("APP"))
+TOKEN = os.getenv("TOKEN")
+
+ADMINS = [int(os.getenv("ID"))]
 
 EMBED_COLOR = 0x000
 DEFAULT_GUILD_SETTINGS = {"lang": "en", "logs": 0, "raid": False}
