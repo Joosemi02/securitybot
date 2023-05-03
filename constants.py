6ce4176ae4db06cb0298e6ffc546bafb885a4031
Joosemi02 <josemiguel.dark@gmail.com>
2023-05-04 01:09:02 +0200

    Format

--- constants.py
+++ constants.py
@@ -8,20 +8,20 @@ MONGODB_CONNECTION_URI = os.getenv("MONGO")
 APPLICATION_ID = int(os.getenv("APP"))
 TOKEN = os.getenv("TOKEN")
 
-ADMINS = [int(os.getenv("ID"))]
+ADMINS = [int(os.getenv("ID1")), int(os.getenv("ID2"))]
 
 EMBED_COLOR = 0x3498DB
 DEFAULT_GUILD_SETTINGS = {
     "lang": "en",
     "logs": 0,
-    "antispam": {"enabled": False, "punishments": [], "notify": 0},
     "suspicious_joins": 0,
+    "antispam": {"enabled": False, "punishments": [], "notify": 0},
+    "linkfilter": {"enabled": False, "punishments": [], "notify": 0},
     "antiraid": {"enabled": False, "punishments": []},
-    "link_filter": {"enabled": False, "punishments": []},
 }
 
 MAX_CLEAR_AMOUNT = 300
 
-BUG_REPORT_CHANNEL = 0
+BUG_REPORT_CHANNEL = int(os.getenv("BUG"))
 
 LANGUAGES = {"English": "en"}
