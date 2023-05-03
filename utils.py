2a1d579d7998d92631fdb3061f9de51e0b44c66b
Joosemi02 <josemiguel.dark@gmail.com>
2023-05-03 20:20:36 +0200

    Made paginator work for both warnings and bans commands

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
+import contextlib
 import json
-import os
 from datetime import datetime
 
-import discord
-from discord import Interaction
+from discord import BanEntry, ButtonStyle, Color, Embed, Guild, Interaction, User
+from discord.errors import NotFound
 from discord.ext import commands
+from discord.ui import View, button
 from discord.utils import format_dt
 from motor import motor_tornado
 
@@ -30,7 +31,7 @@ class MyBot(commands.Bot):
             d["_id"]: {k: v for k, v in d.items() if k != "_id"} async for d in prefs
         }
 
-    async def log(self, object_: Interaction | tuple[int, discord.User], msg: str):
+    async def log(self, object_: Interaction | tuple[int, User], msg: str):
         if isinstance(object_, Interaction):
             guild_id = object_.guild_id
             user = object_.user
@@ -109,7 +110,7 @@ translations = load_languages()
 
 
 def _T(
-    object_: discord.Interaction | commands.Context | discord.Guild | int,
+    object_: Interaction | commands.Context | Guild | int,
     key: str,
     **kwargs,
 ) -> str:
@@ -124,25 +125,25 @@ def _T(
 
 
 def get_guild_id(object_):
-    if isinstance(object_, (discord.Interaction, commands.Context)):
+    if isinstance(object_, (Interaction, commands.Context)):
         return object_.guild.id
-    elif isinstance(object_, discord.Guild):
+    elif isinstance(object_, Guild):
         return object_.id
     else:
         return object_
 
 
 # FORMAT
-def embed_info(message: str) -> discord.Embed:
-    return discord.Embed(description=message, color=EMBED_COLOR)
+def embed_info(message: str) -> Embed:
+    return Embed(description=message, color=EMBED_COLOR)
 
 
-def embed_fail(message: str) -> discord.Embed:
-    return discord.Embed(description=message, color=discord.Color.red())
+def embed_fail(message: str) -> Embed:
+    return Embed(description=message, color=Color.red())
 
 
-def embed_success(message: str) -> discord.Embed:
-    return discord.Embed(description=message, color=discord.Color.green())
+def embed_success(message: str) -> Embed:
+    return Embed(description=message, color=Color.green())
 
 
 # CHECKS
@@ -153,3 +154,167 @@ def is_admin(object_: Interaction | commands.Context):
     elif isinstance(object_, commands.Context):
         ctx = object_
         return ctx.author.id in ADMINS
+
+
+# PAGINATOR
+
+
+class Paginator:
+    def __init__(
+        self,
+        interaction: Interaction,
+        objects: dict[str, set[str, datetime]] | list[BanEntry],
+        **kwargs,
+    ) -> None:
+        self.i: Interaction = interaction
+        self.ITEMS_PER_PAGE = 10
+        self.type_ = "warnings" if isinstance(objects, dict) else "bans"
+        self.objects: dict = objects
+        self.view = (
+            PaginatorView(self.i, self, timeout=30)
+            if len(self.objects) > self.ITEMS_PER_PAGE
+            else None
+        )
+
+        self.page = kwargs.get("page", 1)
+        calculation = len(self.objects) / self.ITEMS_PER_PAGE
+        self.total_pages = (
+            int(calculation) if calculation.is_integer() else int(calculation) + 1
+        )
+        if self.total_pages == 0:
+            self.total_pages = 1
+
+    def _get_cards(self) -> list[dict]:
+        return self.objects[
+            (self.page - 1) * self.ITEMS_PER_PAGE : self.page * self.ITEMS_PER_PAGE
+        ]
+
+    def _build_warnings_embed(self):
+        embed = Embed(
+            description="" if self.objects else _T(self.i, "warnings.display.no_warns"),
+            color=EMBED_COLOR,
+        )
+        for num, dict_ in self.objects.items():
+            reason = f"{_T(self.i, 'warnings.display.reason')}: {dict_[0]}"
+            date = f"{_T(self.i, 'warnings.display.date')}: {format_dt(dict_[1])}"
+            embed.add_field(
+                name=f"ID: {num}",
+                value=f"{reason}\n{date}",
+                inline=False,
+            )
+        return self._set_embed_footer_author(
+            embed, "warnings.display.page", "warnings.display.title"
+        )
+
+    def _build_bans_embed(self):
+        self.objects: list[BanEntry]
+        embed = Embed(
+            description="" if self.objects else _T(self.i, "moderation.bans.no_bans"),
+            color=EMBED_COLOR,
+        )
+        for ban in self.objects:
+            embed.add_field(
+                name=f"ID: {ban.user.id}",
+                value=f"{_T(self.i, 'moderation.bans.reason')}: {ban.reason or '---'}",
+                inline=False,
+            )
+        return self._set_embed_footer_author(
+            embed, "moderation.bans.page", "moderation.bans.title"
+        )
+
+    def _set_embed_footer_author(self, embed, footer_key, author_key):
+        embed.set_footer(
+            text=f"{_T(self.i, footer_key)} {self.page}/{self.total_pages}"
+        )
+        embed.set_author(
+            name=f"{_T(self.i, author_key)} {self.i.user.name}",
+            icon_url=self.i.user.display_avatar.url,
+        )
+        return embed
+
+    @property
+    def embed(self) -> Embed:
+        if self.type_ == "warnings":
+            return self._build_warnings_embed()
+        else:
+            return self._build_bans_embed()
+
+    async def send_message(self, i: Interaction):
+        if not i.command:
+            self.view.update_buttons()
+            await i.followup.edit_message(
+                message_id=i.message.id, embed=self.embed, view=self.view
+            )
+        elif self.view:
+            await i.followup.send(embed=self.embed, view=self.view)
+        else:
+            await i.followup.send(embed=self.embed)
+
+
+class PaginatorView(View):
+    def __init__(self, i, paginator, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.i: Interaction = i
+        self.paginator: Paginator = paginator
+
+    async def on_timeout(self):
+        if self.paginator.i:
+            with contextlib.suppress(NotFound):
+                m = await self.paginator.i.original_response()
+                await m.edit(view=None)
+
+    async def interaction_check(self, interaction: Interaction) -> bool:
+        if interaction.user != self.i.user:
+            await interaction.response.send_message(
+                _T(interaction, "command_fail.not_own_paginator")
+            )
+            return False
+        return True
+
+    def update_buttons(self):
+        last_disable = self.paginator.page <= 1
+        next_disable = self.paginator.page >= self.paginator.total_pages
+        self.children[0].disabled = last_disable
+        self.children[1].disabled = last_disable
+        self.children[-2].disabled = next_disable
+        self.children[-1].disabled = next_disable
+
+    @button(
+        emoji="⏪",
+        custom_id="fastlast",
+        style=ButtonStyle.secondary,
+        disabled=True,
+    )
+    async def fastlast_callback(self, interaction: Interaction, _):
+        self.paginator.page = 1
+        await self.paginator.send_message(interaction)
+
+    @button(emoji="◀", custom_id="last", style=ButtonStyle.secondary, disabled=True)
+    async def last_callback(self, interaction: Interaction, _):
+        self.paginator.page -= 1
+        await self.paginator.send_message(interaction)
+
+    @button(emoji="❌", custom_id="stop", style=ButtonStyle.secondary)
+    async def stop_callback(self, interaction: Interaction, _):
+        await interaction.message.delete()
+        self.stop()
+
+    @button(
+        emoji="▶️",
+        custom_id="next",
+        style=ButtonStyle.secondary,
+        disabled=False,
+    )
+    async def next_callback(self, interaction: Interaction, _):
+        self.paginator.page += 1
+        await self.paginator.send_message(interaction)
+
+    @button(
+        emoji="⏩",
+        custom_id="fastnext",
+        style=ButtonStyle.secondary,
+        disabled=False,
+    )
+    async def fastnext_callback(self, interaction: Interaction, _):
+        self.paginator.page = self.paginator.total_pages
+        await self.paginator.send_message(interaction)
