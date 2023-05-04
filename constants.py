5e268c051739e6ca40c3e5caaef3ccc017df11e8
Joosemi02 <josemiguel.dark@gmail.com>
2023-05-04 11:01:14 +0200

    Move guild prefs to utils

--- constants.py
+++ constants.py
@@ -11,14 +11,6 @@ TOKEN = os.getenv("TOKEN")
 ADMINS = [int(os.getenv("ID1")), int(os.getenv("ID2"))]
 
 EMBED_COLOR = 0x3498DB
-DEFAULT_GUILD_SETTINGS = {
-    "lang": "en",
-    "logs": 0,
-    "suspicious_joins": 0,
-    "antispam": {"enabled": False, "punishments": [], "notify": 0},
-    "linkfilter": {"enabled": False, "punishments": [], "notify": 0},
-    "antiraid": {"enabled": False, "punishments": [], "notify": 0},
-}
 
 MAX_CLEAR_AMOUNT = 300
 
